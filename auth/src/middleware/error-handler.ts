0fe0cbaba9a85b73ee65ee3bdc137682d3984a3a
Sean Fang <fangzhengonly@gmail.com>
2023-06-15 14:11:04 -0400

    add abstract custom error class to normalize the responses for error handling

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const erorrHandler = (
   err: Error,
@@ -9,23 +10,28 @@ export const erorrHandler = (
   next: NextFunction
 ) => {
   // console.log('some error', err);
-  if (err instanceof RequestValidationError) {
-    console.log('request validation error');
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === 'field')
-        return { message: error.msg, field: error.path };
-    });
-    return res.status(400).send({ errors: formattedErrors });
-  }
-  if (err instanceof DatabaseConnectionError) {
-    console.log('database connection error');
-    return res.status(500).send({
-      errors: [
-        {
-          message: err.message,
-        },
-      ],
-    });
+  // if (err instanceof RequestValidationError) {
+  //   console.log('request validation error');
+  // const formattedErrors = err.errors.map((error) => {
+  //   if (error.type === 'field')
+  //     return { message: error.msg, field: error.path };
+  // });
+  // return res.status(400).send({ errors: formattedErrors });
+  //   return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  // }
+  // if (err instanceof DatabaseConnectionError) {
+  //   console.log('database connection error');
+  // return res.status(500).send({
+  //   errors: [
+  //     {
+  //       message: err.message,
+  //     },
+  //   ],
+  // });
+  //   return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  // }
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   res.status(400).send({
