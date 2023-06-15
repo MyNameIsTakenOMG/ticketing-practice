a5e59fdb0c7d29cbe7c516ef1ae69f2418eb6672
Sean Fang <fangzhengonly@gmail.com>
2023-06-15 14:17:34 -0400

    add not-found custom error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { erorrHandler } from './middleware/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(express.json());
@@ -12,11 +13,19 @@ app.use(express.urlencoded({ extended: false }));
 // app.get('/api/users/currentuser', (req, res) => {
 //   res.send('hello world!');
 // });
+
+// routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+//404 error
+app.get('*', (req, res) => {
+  throw new NotFoundError();
+});
+
+// error handler
 app.use(erorrHandler);
 
 app.listen(3000, () => {
