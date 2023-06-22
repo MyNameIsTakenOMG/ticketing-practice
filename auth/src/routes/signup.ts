75a0b70539390fb5b062834858913ed15ab04d40
Sean Fang <fangzhengonly@gmail.com>
2023-06-22 18:08:57 -0400

    add middleware "validate-request" to refactor signup and signin routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,10 +2,9 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
@@ -18,15 +17,16 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     // scenario 1 : Invalid inputs
-    if (!errors.isEmpty()) {
-      // return res.status(400).send(errors.array());
-      // throw new Error('Invalid email or password');
-      throw new RequestValidationError(errors.array());
-    }
+    // if (!errors.isEmpty()) {
+    // return res.status(400).send(errors.array());
+    // throw new Error('Invalid email or password');
+    // throw new RequestValidationError(errors.array());
+    // }
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email: email });
