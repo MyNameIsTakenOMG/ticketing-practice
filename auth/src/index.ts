b5e30a6ea3963ccd83361e660f5c4217c04f0474
Sean Fang <fangzhengonly@gmail.com>
2023-06-23 16:41:47 -0400

    refactor index.ts of auth by creating app.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,43 +1,6 @@
-import express from 'express';
 import 'express-async-errors';
-import mongoose, { mongo } from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { erorrHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-// app.get('/api/users/currentuser', (req, res) => {
-//   res.send('hello world!');
-// });
-
-// routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-//404 error
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-// error handler
-app.use(erorrHandler);
+import mongoose from 'mongoose';
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_SECRET) {
