ef849266cf2811ae9b93d403510c31b2467d2dd2
Sean Fang <fangzhengonly@gmail.com>
2023-06-23 01:06:36 -0400

    implement require-auth middleware and apply it to currentUser route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
 import { currentUserMiddleware } from '../middleware/current-user';
+import { requireAuth } from '../middleware/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUserMiddleware, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
-});
+router.get(
+  '/api/users/currentuser',
+  currentUserMiddleware,
+  requireAuth,
+  (req, res) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
