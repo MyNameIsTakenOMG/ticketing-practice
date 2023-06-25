7a0b8fc39eb96a3c5f5a3e49cd2c865a5522bd14
Sean Fang <fangzhengonly@gmail.com>
2023-06-25 00:41:15 -0400

    add global.signin() helper function

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,14 +2,15 @@ import request from 'supertest'
 import {app} from '../../app'
 
 it('resonses with details of current user', async() => {
-  const authResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-          email: 'user@example.com',
-          password: 'password'
-        })
-        expect(201)
-  const cookie = authResponse.get('Set-Cookie')
+  // const authResponse = await request(app)
+  //       .post('/api/users/signup')
+  //       .send({
+  //         email: 'user@example.com',
+  //         password: 'password'
+  //       })
+  //       expect(201)
+  // const cookie = authResponse.get('Set-Cookie')
+  const cookie = await signin()
 
   const response = await request(app)
                           .get('/api/users/currentuser')
