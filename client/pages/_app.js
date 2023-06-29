3a1fbca45befd873e1fb799a6a263de85f2d7553
Sean Fang <fangzhengonly@gmail.com>
2023-06-29 16:29:25 -0400

    add ssr (getInitialProps) to the custom app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import App from 'next/app';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header !</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  console.log(Object.keys(appContext).toString());
+  const { data } = await buildClient(appContext.ctx).get(
+    '/api/users/currentuser'
+  );
+  console.log(data);
+  return { data, ...appProps };
+};
+
+export default AppComponent;
