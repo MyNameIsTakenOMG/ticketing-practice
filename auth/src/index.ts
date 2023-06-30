5ce514e0a84210c540f382b4e3e131e9b9d48221
Sean Fang <fangzhengonly@gmail.com>
2023-06-30 17:45:32 -0400

    update MONGO_URI for auth service and yml file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,9 +6,12 @@ const start = async () => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT secret is required');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO URI is required');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error);
   }
