a600e8b90918e50071ba3c343b330c51874f23ff
Sean Fang <fangzhengonly@gmail.com>
2023-06-30 19:29:53 -0400

    add Ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+});
+
+ticketSchema.methods.toJSON = function () {
+  const { _id, ...ticket } = this.toObject();
+  ticket.id = _id;
+  return ticket;
+};
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -15,6 +15,33 @@ it('returns a status other than 401 if the user is signed in', async () => {
     .send({});
   expect(response.status).not.toEqual(401);
 });
-it('returns an error if an invalid title is provided', async () => {});
-it('returns an error if an invalid price is provided', async () => {});
-it('creates a ticket with valid input', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({ title: '', price: 10 })
+    .expect(400);
+});
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({ title: 'awfewa' })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', signin())
+    .send({ title: 'awfewa', price: -10 })
+    .expect(400);
+});
+it('creates a ticket with valid input', async () => {
+  // add in a check to make sure a ticket was saved
+  await request(app)
+    .post('/api/tickets')
+    .send({
+      title: 'title1',
+      price: 20,
+    })
+    .expect(201);
+});
