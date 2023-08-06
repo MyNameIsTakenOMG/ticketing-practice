1b50eb40696ce09722f6eae08c6b9630a7d35a48
Sean Fang <fangzhengonly@gmail.com>
2023-08-06 16:38:03 -0400

    setup payment service and define schema and stuff for order model

---
+++ payment/src/models/order.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@sftickets0110/common';
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  price: number;
+  userId: string;
+  status: OrderStatus;
+}
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  price: number;
+  userId: string;
+  status: OrderStatus;
+}
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+orderSchema.methods.toJSON = function () {
+  const { _id, order } = this.toObject();
+  order.id = _id;
+  return order;
+};
+
+orderSchema.statics.build = function (attrs: OrderAttrs) {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model('Order', orderSchema);
